4f9e0c9939430c9473c883817fce8e094648fecb
Rob Siefert <robjsief@gmail.com>
2022-06-07 09:02:10 -0400

    6/7 morning pull

--- module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,51 +1,60 @@
 package com.techelevator;
 
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class WordSearch {
 
-	public static void main(String[] args) {
-		String filename;
+	public static void main(String[] args)  {
+		String fileName;
 		String wordToSearch;
 		Scanner userInput = new Scanner(System.in);
+		boolean caseSensitive;
 
 		System.out.println("What is the fully qualified name of the file that should be searched?");
-		filename = userInput.nextLine();
+		fileName = userInput.nextLine();
 		System.out.println("What word do you want to search for?");
 		wordToSearch = userInput.nextLine();
-
-		Scanner fileScanner = new Scanner(filename);
-		Map<Integer, String> linesInFile = new HashMap<>();
-		int i = 1;
-		while(fileScanner.hasNextLine()){
-			linesInFile.put((Integer)i,fileScanner.nextLine());
-			i++;
+		System.out.println("Should the search be case sensitive? (Y/N");
+		if (userInput.nextLine().toUpperCase().equals("Y")) {
+			caseSensitive = true;
+		} else {
+			caseSensitive = false;
 		}
 
-		Map<Integer, String> linesWithWord = new HashMap<>();
-		Set<Integer> linesInFileKeys = linesInFile.keySet();
 
-		for(Integer key: linesInFileKeys){
-			if (linesInFile.get(key).contains(wordToSearch)) {
-				linesWithWord.put(key,linesInFile.get(key));
+		File file = new File(fileName);
+		try{
+			Scanner fileScanner = new Scanner(file);
+			if (caseSensitive) {
+				int i = 1;
+				while (fileScanner.hasNextLine()) {
+					String currentline = fileScanner.nextLine();
+					if (currentline.contains(wordToSearch)) {
+						System.out.println(i + ") " + currentline);
+					}
+					i++;
+
+				}
+			} else {
+				int i = 1;
+				while (fileScanner.hasNextLine()) {
+					wordToSearch = wordToSearch.toLowerCase();
+					String currentline = fileScanner.nextLine();
+					if (currentline.toLowerCase().contains(wordToSearch)) {
+						System.out.println(i + ") " + currentline);
+					}
+					i++;
+
+				}
 			}
 		}
-		Set<Integer> linesWithWordKeys = linesWithWord.keySet();
+		catch(FileNotFoundException e){
+			System.out.println("Error: File Not Found");
 
-		for (Integer key: linesWithWordKeys) {
-			System.out.println(key +") " + linesWithWord.get(key));
 		}
 
-
-
-
-
-
-
 	}
-
 }
