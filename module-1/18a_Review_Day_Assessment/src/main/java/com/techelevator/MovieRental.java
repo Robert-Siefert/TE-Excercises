aae39f2052419b2f5a399ccf0f2d2966ab3bad5d
Rob Siefert <robjsief@gmail.com>
2022-06-08 09:15:44 -0400

    Step 1 and 2 complete

---
+++ module-1/18a_Review_Day_Assessment/src/main/java/com/techelevator/MovieRental.java
@@ -0,0 +1,63 @@
+package com.techelevator;
+
+public class MovieRental {
+    private String title;
+    private String format;
+    private boolean isPremium;
+    private double rentalPrice;
+
+    public MovieRental(String title, String format, boolean isPremium){
+        this.title = title;
+        this.format = format;
+        this.isPremium = isPremium;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public boolean isPremium() {
+        return isPremium;
+    }
+
+    public void setPremium(boolean premium) {
+        isPremium = premium;
+    }
+
+    public double getRentalPrice() {
+        if (format.equals("VHS")) {
+            rentalPrice = .99;
+        }
+        else if (format.equals("DVD")) {
+            rentalPrice = 1.99;
+        } else if (format.equals("BLUERAY")) {
+            rentalPrice = 2.99;
+        }
+        if (isPremium) {
+            rentalPrice += 1;
+
+
+        }
+        return rentalPrice;
+    }
+
+    public void setRentalPrice(double rentalPrice) {
+        this.rentalPrice = rentalPrice;
+    }
+}
+
+
+
+
