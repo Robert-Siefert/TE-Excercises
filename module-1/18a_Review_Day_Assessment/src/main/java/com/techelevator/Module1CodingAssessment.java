73426ccd559baea4460c8a56ab69310a571efc63
Rob Siefert <robjsief@gmail.com>
2022-06-08 09:29:38 -0400

    step 9 complete

--- module-1/18a_Review_Day_Assessment/src/main/java/com/techelevator/Module1CodingAssessment.java
+++ module-1/18a_Review_Day_Assessment/src/main/java/com/techelevator/Module1CodingAssessment.java
@@ -9,19 +9,26 @@ public class Module1CodingAssessment {
 		Module1CodingAssessment mca = new Module1CodingAssessment();
 		mca.run();
 	}
-
+	List<MovieRental> movieRentals = new ArrayList<>();
 	private void run() {
 		MovieRental superman = new MovieRental("Superman","DVD",false);
 		MovieRental ironMan = new MovieRental("Iron Man","BLUERAY",true);
 		MovieRental citizenKane = new MovieRental("Citizen Kane","VHS",false);
 
 
-		List<MovieRental> movieRentals = new ArrayList<>();
 		movieRentals.add(superman);
 		movieRentals.add(ironMan);
 		movieRentals.add(citizenKane);
 
 
+		printMovieList();
+
+	}
+
+	public void printMovieList(){
+		for (MovieRental movieRental: movieRentals) {
+			System.out.println(movieRental.toString());
+		}
 	}
 
 }
