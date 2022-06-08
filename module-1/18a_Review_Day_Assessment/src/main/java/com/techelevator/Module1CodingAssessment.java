87130a66f8d59183fdaa5d9ca5106b91a83f37bf
Rob Siefert <robjsief@gmail.com>
2022-06-08 09:20:53 -0400

    step 4 and 5 complete

--- module-1/18a_Review_Day_Assessment/src/main/java/com/techelevator/Module1CodingAssessment.java
+++ module-1/18a_Review_Day_Assessment/src/main/java/com/techelevator/Module1CodingAssessment.java
@@ -1,5 +1,8 @@
 package com.techelevator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Module1CodingAssessment {
 
 	public static void main(String[] args) {
@@ -8,6 +11,15 @@ public class Module1CodingAssessment {
 	}
 
 	private void run() {
+		MovieRental superman = new MovieRental("Superman","DVD",false);
+		MovieRental ironMan = new MovieRental("Iron Man","BLUERAY",true);
+		MovieRental citizenKane = new MovieRental("Citizen Kane","VHS",false);
+
+
+		List<MovieRental> movieRentals = new ArrayList<>();
+		movieRentals.add(superman);
+		movieRentals.add(ironMan);
+		movieRentals.add(citizenKane);
 
 
 	}
