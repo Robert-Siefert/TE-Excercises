086b8f658edd56be113a7bb48755e612dc44c6c6
Rob Siefert <robjsief@gmail.com>
2022-06-08 09:53:22 -0400

    Assessment completed 2.0, now with functioning unit tests!

--- module-1/18a_Review_Day_Assessment/src/test/java/com/techelevator/MovieRentalTests.java
+++ module-1/18a_Review_Day_Assessment/src/test/java/com/techelevator/MovieRentalTests.java
@@ -7,12 +7,12 @@ public class MovieRentalTests {
     MovieRental movieRental = new MovieRental("test","DVD",false);
     @Test
     public void testRentalPrice(){
-        Assert.assertEquals(1.99, movieRental.getRentalPrice());
+        Assert.assertEquals(1.99, movieRental.getRentalPrice(), 0.0001);
     }
     @Test
     public void testLateFee(){
-        Assert.assertEquals(0,movieRental.getLateFee(0));
-        Assert.assertEquals(19.99,movieRental.getLateFee(42));
+        Assert.assertEquals(0,movieRental.getLateFee(0), 0.0001);
+        Assert.assertEquals(19.99,movieRental.getLateFee(42), 0.0001);
 
     }
 }
