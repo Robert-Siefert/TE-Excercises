e60b840fdfb209fec85b8782768f25d02bc958d4
Rob Siefert <robjsief@gmail.com>
2022-06-13 18:13:24 -0400

    first SQL excercises

--- module-2/01_Intro_to_Databases/exercise/Exercises/14_new_states.sql
+++ module-2/01_Intro_to_Databases/exercise/Exercises/14_new_states.sql
@@ -1,2 +1,4 @@
 -- 14. The state name, nickname, and census region for states that start with the word "New" (4 rows)
-
+select state_name, state_nickname, Census_region
+from state
+where state_name like 'New%'
