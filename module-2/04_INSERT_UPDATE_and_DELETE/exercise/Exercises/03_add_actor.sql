1c6cc024ce75750f25353369a3199e0805d19e1c
Rob Siefert <robjsief@gmail.com>
2022-06-17 08:33:19 -0400

    ohhhh its IN the computer

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/03_add_actor.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/03_add_actor.sql
@@ -2,12 +2,11 @@
 --Add Eric Stoltz to the list of actors for "Back to the Future" (1 row)
 
 
---insert into person (person_name)
---values('Eric Stoltz')
+
 
 insert into movie_actor(
 	movie_id, 
 	actor_id)
-values(
-	select movie.movie_id from movie where movie.title = 'Back to the Future',
-	select person.person_id from person where person.person_name = 'Eric Stoltz')
+	values(
+		(select  movie.movie_id from movie where movie.title = 'Back to the Future'),
+		(select  person.person_id from person where person.person_name = 'Eric Stoltz'))
