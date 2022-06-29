c16954586688f8ec71e20cf85541224296057845
Rob Siefert <robjsief@gmail.com>
2022-06-29 14:00:59 -0400

    started server side API

--- module-2/13_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/13_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -2,7 +2,10 @@ package com.techelevator.auctions.controller;
 
 import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.dao.MemoryAuctionDao;
+import com.techelevator.auctions.model.Auction;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/auctions")
@@ -11,7 +14,31 @@ public class AuctionController {
     private AuctionDao dao;
 
     public AuctionController() {
+
         this.dao = new MemoryAuctionDao();
+    }
+
+    @RequestMapping(path = "", method = RequestMethod.GET)
+    public List<Auction> list(){
+       return dao.list();
+
+    }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public Auction get(@PathVariable int id){
+        return dao.get(id);
+    }
+
+    @RequestMapping(path = "", method = RequestMethod.POST)
+    public Auction create(Auction auctionToPost){
+
+        try {
+            Auction postedAuction = dao.create(auctionToPost);
+            return  postedAuction;
+        }
+        catch (
+
+        return null;
     }
 
 }
