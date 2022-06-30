0682d683ed3525efd65408a3a527f71a0cc699f6
Rob Siefert <robjsief@gmail.com>
2022-06-30 13:16:42 -0400

    Server side APIs part 2 complete

--- module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -5,6 +5,40 @@ import java.util.Scanner;
 public class Fibonacci {
 
 	public static void main(String[] args) {
+
+		int[] nums = new int[]{ 9, 2, 3, 7, 5, 6, 4, 8, };
+		int maxSoFar = 0;
+		int minSoFar = 999;
+		int rightIndex = -1;
+		int leftIndex = -1;
+		for(int i = 0; i < nums.length; i++){
+			if (nums[i] > maxSoFar) {
+				maxSoFar = nums[i];
+
+			}
+			if (nums[i] < maxSoFar) {
+				rightIndex = i;
+			}
+		}
+		for (int j = nums.length-1;j>=0;j--) {
+			if (nums[j] < minSoFar) {
+				minSoFar = nums[j];
+
+			}
+			if (nums[j] > minSoFar){
+				leftIndex = j;
+			}
+		}
+
+		System.out.println("First index:" + leftIndex + " SecondIndex:" + rightIndex);
+
+
+
+
+
+
+
+
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("What value do you want to check");
 		int numberToCheck = scanner.nextInt();
