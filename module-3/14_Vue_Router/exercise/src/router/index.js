6a9d0504f1b5d0eee17d9d61e60f70cc54eb237b
Rob Siefert <robjsief@gmail.com>
2022-08-02 14:59:54 -0400

    started router

--- module-3/14_Vue_Router/exercise/src/router/index.js
+++ module-3/14_Vue_Router/exercise/src/router/index.js
@@ -1,9 +1,21 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Home from '../views/Home'
+import MyBooks from '../views/MyBooks'
 
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/myBooks',
+    name: 'MyBooks',
+    component: MyBooks
+  }
 
 ];
 
