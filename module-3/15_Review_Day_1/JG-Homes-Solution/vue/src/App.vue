dab22d56b77940cbe7b3cbf1f39f2fa74ee3ae28
Steve Carmichael <steve@techelevator.com>
2022-08-02 13:58:37 -0400

    added final lecture code

---
+++ module-3/15_Review_Day_1/JG-Homes-Solution/vue/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <!-- <div id="nav">
+      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+    </div> -->
+    <the-header />
+    <router-view />
+    <the-footer />
+  </div>
+</template>
+
+<script>
+
+  import TheHeader from './components/TheHeader.vue'
+  import TheFooter from './components/TheFooter.vue'
+
+  export default {
+
+       name: 'App',
+       components: {
+         TheHeader,
+         TheFooter
+       }
+  }
+
+
+</script>
+
---
+++ module-3/15_Review_Day_1/JG-Homes-Solution/vue/src/components/TheFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <p>&#169; Copyright JG Homes - No stealing stuff you Java Blue / .NET nerds</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'the-footer'
+
+}
+</script>
+
+<style>
+
+</style>
