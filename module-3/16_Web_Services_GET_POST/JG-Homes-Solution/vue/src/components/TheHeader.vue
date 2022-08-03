ed22bfdfb18fa89694ed59dd847f42b446a80fe4
Steve Carmichael <steve@techelevator.com>
2022-08-03 14:55:00 -0400

    final lecture code

---
+++ module-3/16_Web_Services_GET_POST/JG-Homes-Solution/vue/src/components/TheHeader.vue
@@ -0,0 +1,53 @@
+<template>
+   <div>
+       <div id = "container">
+            <img id="banner-image" src="../assets/java-green.png" alt="">
+    
+            <nav>
+                 <router-link v-bind:to="{ name: 'home'}">Home</router-link>
+                <a href="#">About</a>
+                <router-link v-bind:to="{ name: 'search'}">Search</router-link>
+                <router-link v-bind:to="{ name: 'addHome'}">Add New Home</router-link>
+                <a href="#">Get Weather</a>
+                <a href="#">Picture of the Day</a>
+            </nav>
+       </div> 
+
+   </div>
+</template>
+
+<script>
+export default {
+    name: 'the-header',
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<style scoped>
+  #container {
+     display: flex;
+     flex-direction: column;
+     justify-content: center;
+     align-items: center;
+  }
+
+  #banner-image {
+      margin-bottom: 40px;
+      width: 10%;
+      height: auto;
+  }
+
+  nav a {
+      padding: 15px 25px;
+      background-color: rgb(3, 189, 3);
+      color:black
+  }
+
+  nav a:hover {
+      background-color: green;
+      color: white;
+  }
+</style>
---
+++ module-3/16_Web_Services_GET_POST/JG-Homes-Solution/vue/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h3 id="intro">Central Ohio's home for finding shack's for JavaScript hacks</h3>
+    <img src="../assets/shack.jpg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home"
+};
+</script>
+
+<style scoped>
+   #intro {
+     margin:30px;
+   }
+</style>
